d7601a2737a01316debd58d25dac6904ab3b1672
U-ANSYS\afox <afox@CHQ3W76COOP426.win.ansys.com>
2016-05-17 08:57:33 -0400

    added some graphix

--- graphics.cpp
+++ graphics.cpp
@@ -5,18 +5,18 @@
 //constructor
 Graphics::Graphics()
 {
-	//int SDL_CreateWindowAndRenderer(int            width,
-        //                        int            height,
-        //                        Uint32         window_flags,
-        //pointer to window pointer          SDL_Window**   window,
-        //pointer to renderer pointer        SDL_Renderer** renderer)
-	SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer);
-	//void SDL_SetWindowTitle(SDL_Window* window, const char* title)
-	SDL_SetWindowTitle(this->_window, "Game");
+  //int SDL_CreateWindowAndRenderer(int            width,
+  //                        int            height,
+  //                        Uint32         window_flags,
+  //pointer to window pointer          SDL_Window**   window,
+  //pointer to renderer pointer        SDL_Renderer** renderer)
+  SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer);
+  //void SDL_SetWindowTitle(SDL_Window* window, const char* title)
+  SDL_SetWindowTitle(this->_window, "Game");
 }
 
 //destructor
 Graphics::~Graphics()
 {
-	SDL_DestroyWindow(this->_window);
+  SDL_DestroyWindow(this->_window);
 }
--- graphics.h
+++ graphics.h
@@ -1,6 +1,9 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <map>
+#include <string>
+
 struct SDL_Window;
 struct SDL_Renderer;
 
@@ -8,10 +11,18 @@ struct SDL_Renderer;
 
 class Graphics{
 public:
-	Graphics();
-	~Graphics();
+  Graphics();
+  ~Graphics();
+
+  /* Load images from sprite sheet into game */
+  SDL_Surface *loadImage(const std::string &spriteSheetFilePath);
+
+  void blitSurface(SDL_Rect* srcRect, SDL_Rect* destRect, SDL_Texture *srcTexture);
+
 private:
-	SDL_Window* _window;
-	SDL_Renderer* _renderer;
+  SDL_Window* _window;
+  SDL_Renderer* _renderer;
+
+  std::map <std::string, SDL_Surface*> _spriteSheets;
 };
 #endif
