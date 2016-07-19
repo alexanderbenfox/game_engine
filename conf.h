a06fa8f37f41a5f733bb558f68797f19412aca59
Alex Fox <alexanderbenfox@gmail.com>
2016-07-18 20:56:18 -0400

    l

--- conf.h
+++ conf.h
@@ -1,6 +1,31 @@
 #ifndef CONF_H
 #define CONF_H
 
+namespace globals{
+}
+
+namespace sides{
+  enum Side{
+    TOP, BOTTOM, LEFT, RIGHT, NONE
+  };
+  
+  inline Side getOppositeSide(Side side)
+  {
+    return side == TOP ? BOTTOM : side == BOTTOM ? TOP : side == RIGHT ? LEFT : side == LEFT ? RIGHT : NONE;
+  }
+}
+
+enum Direction {
+  LEFT, RIGHT, UP, DOWN
+};
+
+struct Vector2{
+  int x, y;
+  Vector2(): x(0), y(0) {}
+  Vector2(int x, int y): x(x), y(y) {}
+  Vector2 zero() { return Vector2(0,0); }
+};
+
 
 // we use a fixed timestep of 1 / (60 fps) = 16 milliseconds
 #define TIMESTEP 16.0 //in milliseconds
@@ -15,3 +40,4 @@
 #define SPRITE_SCALE 1
 
 #endif
+
